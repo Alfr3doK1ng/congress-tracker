4f68ddc223f254a7224dd2aaa5c2edb17312734d
Alex Rowe <alexrowe.biz@gmail.com>
2024-03-10 18:48:06 -0500

    Added multithreaded processing for pdf parser

--- pdfParser.py
+++ pdfParser.py
@@ -4,31 +4,16 @@ import requests
 from numpy import *
 from pandas import *
 from PyPDF2 import PdfReader
+from multiprocessing import Pool, freeze_support
 
-filename = "2024FD_Alex.txt"
-docID = []
-urls = []
-
-with open(filename) as file:
-    lines = file.readlines()
-    line_count = len(lines)
-
-count = 0
-for i in lines:
-    parts = lines[count].split()
-    docID.append(parts[-1])
-
-    url = "https://disclosures-clerk.house.gov/public_disc/ptr-pdfs/2024/" + str(docID[count]) + ".pdf"
-    urls.append(url)
-    count += 1
-
-for url in urls:
+# 
+def extract_text_from_pdf(url):
    try:
        response = requests.get(url)
        response.raise_for_status()
    except requests.exceptions.RequestException as e:
        print(f"Error fetching PDF from {url}: {e}")
-       continue
+       return None
 
    pdf_file = BytesIO(response.content)
    pdf_reader = PdfReader(pdf_file)
@@ -39,4 +24,39 @@ for url in urls:
        page_text = page.extract_text()
        all_page_text += page_text
 
-   print(f"Text from {url}:\n{all_page_text}")
+   return all_page_text
+
+def main():
+    filename = "2024FD_Alex.txt"
+    docID = []
+    urls = []
+
+    with open(filename) as file:
+        lines = file.readlines()
+        line_count = len(lines)
+
+    count = 0
+    for i in lines:
+        parts = lines[count].split()
+        docID.append(parts[-1])
+
+        url = "https://disclosures-clerk.house.gov/public_disc/ptr-pdfs/2024/" + str(docID[count]) + ".pdf"
+        urls.append(url)
+        count += 1
+
+    return urls
+
+urls = main()
+
+if __name__ == '__main__':
+    freeze_support()
+
+    with Pool() as pool:
+        results = pool.map(extract_text_from_pdf, urls)
+
+    for url, text in zip(urls, results):
+        if text:
+            print(f"Text from {url}:\n{text}")
+        else:
+            print(f"Failed to extract text from {url}")
+    

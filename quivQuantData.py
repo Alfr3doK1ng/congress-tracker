7d2d6b44c04d3a95c20269de15ba33ddd44f1462
Alfred <hlong25@wisc.edu>
2024-03-17 13:46:08 -0500

    Initial backtesting added

--- quivQuantData.py
+++ quivQuantData.py
@@ -6,29 +6,37 @@ import quiverquant
 key = "https://api.quiverquant.com"
 quiver = quiverquant.quiver(key)
 
-df = quiver.congress_trading()
-df = df[df['TransactionDate'] >= '2023-01-01']
+
+# df = quiver.congress_trading()
+# df.to_csv('quiverDF.csv', index=False)
+df = pd.read_csv('quiverDF.csv')
+df = df[df['TransactionDate'] >= '2024-01-01']
 print(df)
 
 alist = []
 for index, row in df.iterrows():
     try:
+        print(row)
         startdate = row[1]
-        startprice = yf.download(row[2], start = startdate, end = startdate + pd.Timedelta(days = 1))
-        endprice = yf.donwload(row[2], start = '2023-03-02', end = '2023-03-03')
+        startprice = yf.download(row[3], start = pd.to_datetime(startdate), end = pd.to_datetime(startdate) + pd.Timedelta(days = 1))
+        endprice = yf.download(row[3], start = pd.to_datetime(startdate) + pd.Timedelta(days = 1) , end = pd.to_datetime(startdate) + pd.Timedelta(days = 2))
+        print(startprice)
+        print(endprice)
 
         startprice = float(startprice['Close'])
         endprice = float(endprice['Close'])
         pot_change = ((endprice - startprice)/startprice)*100
 
         alist.append(pot_change)
-    except:
-        alist.append('skipped')
+    except Exception as e:
+        # alist.append('skipped')
+        alist.append(0)
+        print(f"An error occurred: {e}")
 
 df['return_after_trade'] = alist
-
 
 purchase_df = df[df['Transaction'] == 'Purchase'].sort_values('return_after_trade', ascending = False)
 
 csv_filename = 'purchase_data.csv'
 purchase_df.to_csv(csv_filename)
+print('total return is: ', purchase_df['return_after_trade'].sum())
